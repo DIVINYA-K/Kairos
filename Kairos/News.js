0729593370884c344b5dbc68aad0e15be57dcfd6
DIVINYA-K <cyahillside@gmail.com>
2023-10-16 11:59:57 +1300

    Data

--- Kairos/News.js
+++ Kairos/News.js
@@ -1,9 +1,21 @@
+        //Defining variables
         var question = document.querySelector('#question')
         var choicesElement = document.querySelector('#choices')
-        var userChoices = [];
         var score = 0
+        var userChoices = [];
         var quizQuestion = document.getElementById("quizQuestion"); 
+        //Question Data is my array of questions and choices. This is my first set of Questions that is the same for everthing.
         var questionData = [
+            {
+                question: "How many Bentos would you like?",
+                choices: [
+                    {
+                        type: 'text',
+                        id: 'quantityInput',
+                    }
+                ]
+                
+            }, 
             {
                 question: "Dietary Requirements?",
                 choices: [
@@ -63,9 +75,11 @@
                     },
 
                 ]
-            }
+            },
+                   
         ]
 
+        //This is my second questions data. These set of questions will only appear depending on which heading the user chooses in the previous question.
 
         var questionData2 = [
             {
@@ -162,32 +176,76 @@
             }
 
         ]
+        //This is declaring the variable questionState and setting it's initial value to 0.
         var questionState = 0
 
+        //This is to make the first question in the Question Data array show in the inner HTML and then call the showChoices function.
         quizQuestion.innerHTML = questionData [questionState]['question'];
         showChoices();
 
+        
         function showChoices() {
+            //This clears the previous sets of choices before the new set for the next question shows up.
             choicesElement.innerHTML = "";
+            
             questionData[questionState].choices.forEach(choice => {
-                var choiceButton = document.createElement('button');
-                choiceButton.textContent = choice.text;
-                choicesElement.appendChild(choiceButton);
-                choiceButton.addEventListener ('click',function() {
-                    var clickedChoice = choice.text;
-                    userChoices.push(clickedChoice);
-                    choiceQuestion(clickedChoice);
-                    questionState++;
-                    if (questionState < questionData.length) {
-                        quizQuestion.innerHTML = questionData[questionState].question;
-                        showChoices()
-                    }
-                })
+                if (choice.type === 'text') {
+                    
+                    var quantityInput = document.createElement('input');
+                    quantityInput.type = 'number';
+                    quantityInput.style.height = '40px';
+                    quantityInput.style.width = '40px';
+                    quantityInput.style.fontSize = '20px';
+                    quantityInput.id = choice.id;
+                    choicesElement.appendChild(quantityInput);
+        
+                    
+                    var nextButton = document.createElement('button');
+                    nextButton.textContent = 'Next';
+                    nextButton.addEventListener('click', function () {
+                        var quantity = quantityInput.value;
+                        if (quantity !== "") {
+                            if (quantity > 0 && quantity < 5) {
+                                userChoices.push('Quantity: ' + quantity);
+                                questionState++;
+                    
+                                if (questionState < questionData.length) {
+                                    quizQuestion.innerHTML = questionData[questionState].question;
+                                    showChoices();
+                                }
+                            } else if (quantity < 0) {
+                                alert ("Quantity Invalid")
+                            }
+                            
+                            else {
+                                alert("Please enter a quantity less than 5");
+                            }
+                        } else {
+                            alert("Please enter a quantity.");
+                        }
+                    });
+                       
+        
+                    choicesElement.appendChild(nextButton);
+                } else {
+                    var choiceButton = document.createElement('button');
+                    choiceButton.textContent = choice.text;
+                    choicesElement.appendChild(choiceButton);
+                    choiceButton.addEventListener('click', function () {
+                        var clickedChoice = choice.text;
+                        choiceQuestion(clickedChoice);
+                        questionState++;
+        
+                        if (questionState < questionData.length) {
+                            quizQuestion.innerHTML = questionData[questionState].question;
+                            showChoices();
+                        }
+                    });
+                }
             });
         }
 
         
-
         function showChoices2() {
             var questionState = 0
             choicesElement.innerHTML = "";
@@ -195,9 +253,18 @@
                 var choiceButton = document.createElement('button');
                 choiceButton.textContent = choice.text;
                 choicesElement.appendChild(choiceButton);
-             
-            });
-        }
+                choiceButton.addEventListener("click", function () {
+                    var clickedChoice = choice.text;
+                    userChoices.push(clickedChoice);
+                    
+
+                    quizQuestion.style.display = "none";
+                    choicesElement.style.display = "none";
+                      displayUserChoices();
+                  });
+                });
+              }
+              
 
         function showChoices3() {
             var questionState2 = 1
@@ -206,9 +273,23 @@
                 var choiceButton = document.createElement('button');
                 choiceButton.textContent = choice.text;
                 choicesElement.appendChild(choiceButton);
-             
-            });
-        }
+                choiceButton.addEventListener("click", function () {
+                    var clickedChoice = choice.text;
+                    userChoices.push(clickedChoice);
+              
+                    questionState++;
+              
+                    if (questionState < questionData2.length) {
+                      quizQuestion.innerHTML = questionData2[questionState].question;
+                      showChoices3();
+                    } else {
+                        quizQuestion.style.display = "none";
+                        choicesElement.style.display = "none";
+                      displayUserChoices();
+                    }
+                  });
+                });
+              }
 
         function showChoices4() {
             var questionState3 = 2
@@ -217,9 +298,23 @@
                 var choiceButton = document.createElement('button');
                 choiceButton.textContent = choice.text;
                 choicesElement.appendChild(choiceButton);
-             
-            });
-        }
+                choiceButton.addEventListener("click", function () {
+                    var clickedChoice = choice.text;
+                    userChoices.push(clickedChoice);
+              
+                    questionState++;
+              
+                    if (questionState < questionData2.length) {
+                      quizQuestion.innerHTML = questionData2[questionState].question;
+                      showChoices4();
+                    } else {
+                        quizQuestion.style.display = "none";
+                        choicesElement.style.display = "none";
+                      displayUserChoices();
+                    }
+                  });
+                });
+              }
 
         
         function showChoices5() {
@@ -229,67 +324,64 @@
                 var choiceButton = document.createElement('button');
                 choiceButton.textContent = choice.text;
                 choicesElement.appendChild(choiceButton);
-             
-            });
-        }
-        
+                choiceButton.addEventListener("click", function () {
+                    var clickedChoice = choice.text;
+                    userChoices.push(clickedChoice);
+              
+                    questionState++;
+              
+                    if (questionState < questionData2.length) {
+                      quizQuestion.innerHTML = questionData2[questionState].question;
+                      showChoices5();
+                    } else {
+                        quizQuestion.style.display = "none";
+                        choicesElement.style.display = "none";
+                      displayUserChoices();
+                    }
+                  });
+                });
+              }
 
 
         function choiceQuestion(clickedChoice) {
-
-                if (clickedChoice === "Sushi") {
-
-                    var questionState = 0
-
-                    quizQuestion.innerHTML = questionData2 [questionState]['question'];
-                    showChoices2(); 
-                    
-                }
-                else if (clickedChoice==="Wrap") {
-                        var questionState = 1
-
-                    quizQuestion.innerHTML = questionData2 [questionState]['question'];
-                    showChoices3();
-                }
-                  
-                    else if (clickedChoice==="Salad") {
-                        var questionState = 2
-
-                    quizQuestion.innerHTML = questionData2 [questionState]['question'];
-                    showChoices4();
-                    }
-                
-                    else if (clickedChoice==="Pizza") {
-                        var questionState = 3
-
-                    quizQuestion.innerHTML = questionData2 [questionState]['question'];
-                    showChoices5();
-                    }
-
-                    else {
-                        displayUserChoices();
-                        saveUserChoicesToFirebase();
-                      }
-
+            userChoices.push(clickedChoice);
+        
+            if (clickedChoice === "Sushi") {
+                var questionState = 0;
+                quizQuestion.innerHTML = questionData2[questionState]['question'];
+                showChoices2();
+            } 
+            else if (clickedChoice === "Wrap") {
+                var questionState = 1;
+                quizQuestion.innerHTML = questionData2[questionState]['question'];
+                showChoices3();
+            } 
+            else if (clickedChoice === "Salad") {
+                var questionState = 2;
+                quizQuestion.innerHTML = questionData2[questionState]['question'];
+                showChoices4();
+            } 
+            else if (clickedChoice === "Pizza") {
+                var questionState = 3;
+                quizQuestion.innerHTML = questionData2[questionState]['question'];
+                showChoices5();
+            }
+            
         }
+        
 
         function displayUserChoices(){
             var userChoicesContainer = document.getElementById("userChoicesContainer");
-            userChoicesContainer.innerHTML = "<h2>Your Choices:</h2>";
+            userChoicesContainer.innerHTML = "<h2>You have picked:</h2>";
             userChoices.forEach(choice => {
                 var choiceElement = document.createElement("p");
                 choiceElement.textContent = choice;
                 userChoicesContainer.appendChild(choiceElement);
             });
         }
+        
 
-        function saveUserChoicesToFirebase() {
-            var userChoicesRef = database.ref("userChoices");
 
-            userChoicesRef.push ({
-                choices: userChoices
-            });
-         }
 
         
     
